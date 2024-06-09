f92b7607b4fa8cacd5eeaa39ed1688e7b7ca1e38
I-am-ndbk <165913997+I-am-ndbk@users.noreply.github.com>
2024-06-09 11:35:30 +0700

    NetTruyen.ts

--- src/NetTruyen/NetTruyen.ts
+++ src/NetTruyen/NetTruyen.ts
@@ -17,7 +17,7 @@ import {
 } from "paperback-extensions-common";
 import { Parser } from "./NetTruyenParser";
 
-const DOMAIN = "https://www.nettruyenus.com/";
+const DOMAIN = "https://nettruyencc.com/";
 
 export const isLastPage = ($: CheerioStatic): boolean => {
   const current = $("ul.pagination > li.active > a").text();
